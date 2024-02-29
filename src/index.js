a2ee71d9307ec70005e7070845f732c668259269
esel-fliegen <esel.fliegen@gmail.com>
2024-02-29 16:11:39 +0700

    auth

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import * as ReactDOM from "react-dom/client"
 import { Provider } from "react-redux";
 import ApplicationComponent from "./component/app/app";
 import store from "./state/store";
+import { Amplify } from 'aws-amplify';
+import config from './aws-exports';
+Amplify.configure(config);
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
