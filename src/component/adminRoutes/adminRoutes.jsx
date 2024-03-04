7430fb6157848b6c5e5eb47fc2b67d209858ab53
esel-fliegen <esel.fliegen@gmail.com>
2024-03-05 01:55:39 +0700

    routes

--- src/component/adminRoutes/adminRoutes.jsx
+++ src/component/adminRoutes/adminRoutes.jsx
@@ -1,41 +1,41 @@
 import React, { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Routes, Route, useRoutes, Outlet, Navigate,} from "react-router-dom";
-export const Page404 = lazy(() => import('../pages/pageNotFound.jsx'));
-
+//export const Page404 = lazy(() => import('../pages/pageNotFound.jsx'));
+import NotFoundPage from '../pages/pageNotFound.jsx';
 // ----------------------------------------------------------------------
 
 export default function AdminRouter() {
 
-  // return (
-  //   <Routes>
-  //     <Route path="*" element={<Page404/>}/>
+  return (
+    <Routes>
+      <Route path="*" element={<NotFoundPage/>}/>
 
-  //   </Routes>
-  // )
-  const routes = useRoutes([
-    {
-      element: (
-        {/* <DashboardLayout>
-          <Suspense>
-            <Outlet />
-          </Suspense>
-        </DashboardLayout> */}
-      ),
-      children: [
-       // { element: <IndexPage />, index: true },
-        //{ path: 'admin', element: <AdminPage /> },
+    </Routes>
+  )
+  // const routes = useRoutes([
+  //   {
+  //     element: (
+  //       {/* <DashboardLayout>
+  //         <Suspense>
+  //           <Outlet />
+  //         </Suspense>
+  //       </DashboardLayout> */}
+  //     ),
+  //     children: [
+  //      // { element: <IndexPage />, index: true },
+  //       //{ path: 'admin', element: <AdminPage /> },
        
-      ],
-    },
-    {
-      path: '404',
-      element: <Page404 />,
-    },
-    {
-      path: '*',
-      element: <Navigate to="/404" replace />,
-    },
-  ]);
+  //     ],
+  //   },
+  //   {
+  //     path: '404',
+  //     element: <Page404 />,
+  //   },
+  //   {
+  //     path: '*',
+  //     element: <Navigate to="/404" replace />,
+  //   },
+  // ]);
 
-  return routes;
+  // return routes;
 }
--- src/component/userRoutes/userRoutes.jsx
+++ src/component/userRoutes/userRoutes.jsx
@@ -1,44 +1,43 @@
 import React, { lazy, Suspense } from 'react';
-import { Outlet, Navigate, useRoutes } from 'react-router-dom';
-
+import { BrowserRouter as Router, Routes, Route, useRoutes, Outlet, Navigate,} from "react-router-dom";
 import DashboardComponent from '../dashboard/dashboard.jsx';
-export const Page404 = lazy(() => import('../pages/pageNotFound.jsx'));
-
+//export const Page404 = lazy(() => import('../pages/pageNotFound.jsx'));
+import NotFoundPage from '../pages/pageNotFound.jsx';
 // ----------------------------------------------------------------------
 
 export default function UserRouter({isAdmin}) {
 
-  // return (
-  //   <Routes>
-  //     <Route path="*" element={<Page404/>}/>
+  return (
+    <Routes>
+      <Route path="*" element={<NotFoundPage/>}/>
 
-  //   </Routes>
-  // )
-  const routes = useRoutes([
-    {
-      element: (
+    </Routes>
+  )
+  // const routes = useRoutes([
+  //   {
+  //     element: (
         
-        <DashboardComponent>
-          <Suspense>
-            <Outlet />
-          </Suspense>
-        </DashboardComponent>
-      ),
-      children: [
-       // { element: <IndexPage />, index: true },
-        //{ path: 'admin', element: <AdminPage /> },
+  //       <DashboardComponent>
+  //         <Suspense>
+  //           <Outlet />
+  //         </Suspense>
+  //       </DashboardComponent>
+  //     ),
+  //     children: [
+  //      // { element: <IndexPage />, index: true },
+  //       //{ path: 'admin', element: <AdminPage /> },
        
-      ],
-    },
-    {
-      path: '404',
-      element: <Page404 />,
-    },
-    {
-      path: '*',
-      element: <Navigate to="/404" replace />,
-    },
-  ]);
+  //     ],
+  //   },
+  //   {
+  //     path: '404',
+  //     element: <Page404 />,
+  //   },
+  //   {
+  //     path: '*',
+  //     element: <Navigate to="/404" replace />,
+  //   },
+  // ]);
 
-  return routes;
+  // return routes;
 }
