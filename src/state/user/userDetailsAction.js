8e8209c14950fb04fe0494ca7368eb1bbb282d1a
esel-fliegen <esel.fliegen@gmail.com>
2024-03-08 16:30:25 +0700

    fixed attributes

--- src/state/user/userDetailsAction.js
+++ src/state/user/userDetailsAction.js
@@ -32,7 +32,7 @@ export const SaveUserDetailsToDB = (userDetails)=>{
         if (error.response && [301, 302].includes(error.response.status)) {
           const redirectUrl = error.response.headers.location;
           console.log("1: " + JSON.stringify(userDetails))
-          axiosInstance3.put("http://ec2-54-252-239-111.ap-southeast-2.compute.amazonaws.com:8080/userDetails/update", userDetails)
+          axiosInstance3.post("http://ec2-54-252-239-111.ap-southeast-2.compute.amazonaws.com:8080/userDetails/update", userDetails, header)
           .then((data)=>{
             let updated = data.data;
             console.log("UPDATED " + updated)
