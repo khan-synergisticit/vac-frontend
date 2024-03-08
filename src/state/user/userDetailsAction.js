03167036a1e9557bbde4c7629e7fcab2dac7f8e5
esel-fliegen <esel.fliegen@gmail.com>
2024-03-08 14:59:36 +0700

    fixed attributes

--- src/state/user/userDetailsAction.js
+++ src/state/user/userDetailsAction.js
@@ -16,59 +16,40 @@ export const SaveUserDetailsToDB = (userDetails)=>{
   
  
   let userID = JSON.stringify(userDetails.userID);
-  console.log("userDetails.userID " + userID);
-  let header ={
-     headers: {
-         'Content-Type': 'application/x-www-form-urlencoded',
-         "Access-Control-Allow-Origin": "*",
-     }
-   }
- return (dispatch) =>{
-  axiosInstance.defaults.maxRedirects = 0; 
+  return (dispatch)=>{
+    axiosInstance.defaults.maxRedirects = 0; 
     axiosInstance.interceptors.response.use(
       response => response,
       error => {
       
         if (error.response && [301, 302].includes(error.response.status)) {
           const redirectUrl = error.response.headers.location;
-          console.log("Add UserDetails 2 to store: " + JSON.stringify(error.response.data));
+          console.log("Add UserDetails to store: " + JSON.stringify(error.response.data));
           dispatch(AddUserDetailsToStore(error.response.data))
           return axiosInstance.get(redirectUrl);
         }
         return Promise.reject(error);
       }
     );
-   //axiosInstance.get(`http://localhost:8080/userDetails/find?userID=${userDetails.userID}`)
-   axiosInstance.get(`http://ec2-54-252-239-111.ap-southeast-2.compute.amazonaws.com:8080/userDetails/find?userID=${userID}`)
  
-   .then((data)=>{
-    console.log("DATA: " + JSON.stringify(data.data))
-    /*  if(data.data == null){
-       
-       //axiosInstance.post("http://localhost:8080/userDetails/save", userDetails, header)
-       axios.post("http://ec2-54-252-239-111.ap-southeast-2.compute.amazonaws.com:8080/userDetails/save", userDetails, header)
-       .then((data)=>{
-         let savedDetails = data.data;
-         dispatch(fetchUserDetailsFromDB(savedDetails.userID));
-       })
-       .catch((error)=>{
-         console.log("Save User details to DB Error: " + error)
-       })
-     } else {
-       //axiosInstance.put("http://localhost:8080/userDetails/update", userDetails, header)
-       axios.put("http://ec2-54-252-239-111.ap-southeast-2.compute.amazonaws.com:8080/userDetails/update", userDetails, header)
-       .then((data) => {
-         let savedUser = data.data;
-         dispatch(FetchUserFromDB(savedUser.userID)) 
-       })
-       .catch((error)=>{
-         console.log("Update User details to DB Error: " + error)
-       })
-     } */
-   }).catch((error)=>{
-    console.log("Fetch user from DB Error: " + error);
-  });
- }
+    let header ={
+      headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+          "Access-Control-Allow-Origin": "*",
+      }
+    }
+    console.log("fetchUserDetailsFromDB : " + JSON.stringify(userDetails))
+    //axiosInstance.get(`http://localhost:8080/userDetails/find?userID=${userDetails}`)
+    axiosInstance.get(`http://ec2-54-252-239-111.ap-southeast-2.compute.amazonaws.com:8080/userDetails/find?userID=${userID}`)
+    .then((data)=>{
+      let details = data.data;
+      console.log("Fet userDetails: " + JSON.stringify(data))
+      dispatch(AddUserDetailsToStore(details));
+    })
+    .catch((error)=>{
+      console.log("Fetch user from DB Error: " + error);
+    });
+  }
 }
 
 
