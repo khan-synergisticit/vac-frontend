8250b5770dfe824aeba092dd433017ad56cb5df0
esel-fliegen <esel.fliegen@gmail.com>
2024-03-09 10:55:49 +0700

    mongodb integration

--- src/state/user/userAction.js
+++ src/state/user/userAction.js
@@ -1,8 +1,8 @@
 import * as ActionType from "../actionTypes";
 import axios from "axios";
 
-const axiosInstance = axios.create({baseURL:"http://ec2-54-252-239-111.ap-southeast-2.compute.amazonaws.com:8080/"});
-//const axiosInstance = axios.create({baseURL:"http://localhost:8080/"});
+//const axiosInstance = axios.create({baseURL:"http://ec2-54-252-239-111.ap-southeast-2.compute.amazonaws.com:8080/"});
+const axiosInstance = axios.create({baseURL:"http://localhost:8080/"});
 
 
 export const AddUserToStore = (newUser) => {
@@ -17,36 +17,20 @@ export const AddUserToStore = (newUser) => {
 
 export const SaveUserToDB = (newUser)=>{
   return (dispatch)=>{
-    axiosInstance.post("users/find", newUser)
-    .then((data)=>{
-      if(data.data == null){
-  
-        axiosInstance.post("users/save", newUser)
+    axiosInstance.post("users/save", newUser)
         .then((data) => {
           let savedUser = data.data;
-          dispatch(FetchUserFromDB(savedUser.userID)) 
+          dispatch(FetchUserFromDB(savedUser)) 
         })
         .catch((error)=>{
           console.log("Save User to DB Error: " + error)
         })
-      } else {
-        axios.put("users/update", newUser)
-        .then((data) => {
-          let savedUser = data.data;
-          dispatch(FetchUserFromDB(savedUser.userID)) 
-        })
-        .catch((error)=>{
-          console.log("Update User to DB Error: " + error)
-        })
-      }
-    })
     
   }
 }
 
 export const FetchUserFromDB = (user) =>{
-  return (dispatch)=>{
-    
+  return (dispatch)=>{   
  
     let header ={
       headers: {
@@ -61,9 +45,13 @@ export const FetchUserFromDB = (user) =>{
       dispatch(AddUserToStore(user));
     })
     .catch((error)=>{
+      console.log("User fetch error: " + error)
       if(error.response.status == 301 || error.response.status == 302 ){
         
         dispatch(AddUserToStore(error.response.data))
+      } else if(error.response.status == 404 ){
+        console.log("User fetch 404 error: " + error)
+        dispatch(SaveUserToDB(user));
       } else {
         console.log("Fetch user from DB Error: " + error);
       }
